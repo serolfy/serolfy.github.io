c1e9d54ce1297b0ef545c84d396c892903832b81
Yair Flores <63037678+serolfy@users.noreply.github.com>
2026-02-17 10:28:38 -0500

    Add Clarity initialization in _app.tsx
    Initialize Clarity on the client side if NEXT_CLARITY_ID is set.

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,24 @@
 import "@/styles/globals.css";
+import { useEffect } from "react";
+import Clarity from "@microsoft/clarity";
+import Head from "next/head";
 
 import Layout from "../components/layout";
 
-import { AppProps } from 'next/app';
+import { AppProps } from "next/app";
 
 export default function App({ Component, pageProps }: AppProps) {
+  useEffect(() => {
+    // Only run on the client side
+    if (typeof window !== "undefined" && process.env.NEXT_CLARITY_ID) {
+      Clarity.init(process.env.NEXT_CLARITY_ID);
+    }
+  }, []);
   return (
     <Layout>
+      <Head>
+        <title>Yair Flores</title>
+      </Head>
       <Component {...pageProps} />
     </Layout>
   );
